1e3f8e2be7304174be49f1553e18ebc43a46f9c1
nardosteve <jackcale50@gmail.com>
2023-04-13 13:01:58 +0300

    Creating DB

--- db_connection/connection.php
+++ db_connection/connection.php
@@ -7,11 +7,23 @@ $password = "";
 //creating a connection
 $conn = new mysqli($servername, $username, $password);
 
+//Check connection
 if($conn->connect_error){
     die("Connection failed: " .$conn->connect_error);
 }else{
     echo "Connected successfully";
-    $conn->close();
 }
+
+//Create database
+$sqlCommand = "CREATE DATABASE my_form";
+
+//Check Query
+if($conn->query($sqlCommand) === TRUE){
+    echo "Database Created!";
+}else{
+    echo "ERROR Creating Database: " .$conn->error;
+}
+
+$conn->close();
 
 ?>
--- form-control/form.php
+++ form-control/form.php
@@ -1,19 +1,19 @@
 <div class="container md">
     <form action="">
         <div class="row">
-            <div class="col-6">
+            <div class="col-md-6 col-sm-12">
                 <label for="email" class="form-label">Email address</label>
                 <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
             </div>
-            <div class="col-6">
+            <div class="col-md-6 col-sm-12">
                 <label for="names" class="form-label">Full Names</label>
                 <input type="text" class="form-control" id="names" placeholder="John Doe">
             </div>
-            <div class="col-6">
+            <div class="col-md-6 col-sm-12">
                 <label for="formFile" class="form-label">Upload image</label>
                 <input class="form-control" type="file" id="formFile">
             </div>
-            <div class="col-6 mb-3">
+            <div class="col-md-6 col-sm-12 mb-3">
                 <label for="formFile" class="form-label">Select country</label>
                 <select class="form-select" aria-label="Default select example">
                 <option selected>Country</option>
