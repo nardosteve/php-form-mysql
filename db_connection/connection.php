169f1afd1ee54b328ddd6e19cf71431d07f45966
nardosteve <jackcale50@gmail.com>
2023-04-13 15:03:24 +0300

    Creating tables in the database

--- db_connection/connection.php
+++ db_connection/connection.php
@@ -3,9 +3,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "my_form";
 
 //creating a connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 //Check connection
 if($conn->connect_error){
@@ -14,15 +15,31 @@ if($conn->connect_error){
     echo "Connected successfully";
 }
 
+//Create tables
+$sql = "CREATE TABLE userForm(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    emailAddress VARCHAR(30) NOT NULL,
+    fullNames VARCHAR(60) NOT NULL,
+    uploadImage VARCHAR(60) NOT NULL,
+    country VARCHAR(30) NOT NULL,
+    uploadDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Successfully Created!";
+  } else {
+    echo "Error creating table: " . $conn->error;
+  }
+
 //Create database
-$sqlCommand = "CREATE DATABASE my_form";
+// $sqlCommand = "CREATE DATABASE my_form";
 
 //Check Query
-if($conn->query($sqlCommand) === TRUE){
-    echo "Database Created!";
-}else{
-    echo "ERROR Creating Database: " .$conn->error;
-}
+// if($conn->query($sqlCommand) === TRUE){
+//     echo "Database Created!";
+// }else{
+//     echo "ERROR Creating Database: " .$conn->error;
+// }
 
 $conn->close();
 
