f467133574b1f20f560dab8924cfaff57e01259c
nardosteve <jackcale50@gmail.com>
2023-04-13 11:28:18 +0300

    Intergrated Bootstrap

---
+++ form-control/form.php
@@ -0,0 +1,25 @@
+<div class="container md">
+    <div class="row">
+        <div class="col-6">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="col-6">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="col-6">
+            <label for="formFile" class="form-label">Upload image</label>
+            <input class="form-control" type="file" id="formFile">
+        </div>
+        <div class="col-6">
+            <label for="formFile" class="form-label">Select country</label>
+            <select class="form-select" aria-label="Default select example">
+            <option selected>Country</option>
+            <option value="1">Kenya</option>
+            <option value="2">Uganda</option>
+            <option value="3">Burundi</option>
+            </select>
+        </div>
+    </div>
+</div>
