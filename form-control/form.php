13c385d67b24100b9f0b299bee848856db02fa8d
nardosteve <jackcale50@gmail.com>
2023-04-13 11:45:05 +0300

    btn block

--- form-control/form.php
+++ form-control/form.php
@@ -1,25 +1,30 @@
 <div class="container md">
-    <div class="row">
-        <div class="col-6">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    <form action="">
+        <div class="row">
+            <div class="col-6">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
+            </div>
+            <div class="col-6">
+                <label for="names" class="form-label">Names</label>
+                <input type="text" class="form-control" id="names" placeholder="John Doe">
+            </div>
+            <div class="col-6">
+                <label for="formFile" class="form-label">Upload image</label>
+                <input class="form-control" type="file" id="formFile">
+            </div>
+            <div class="col-6 mb-3">
+                <label for="formFile" class="form-label">Select country</label>
+                <select class="form-select" aria-label="Default select example">
+                <option selected>Country</option>
+                <option value="1">Kenya</option>
+                <option value="2">Uganda</option>
+                <option value="3">Burundi</option>
+                </select>
+            </div>
         </div>
-        <div class="col-6">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-primary">Submit</button>
         </div>
-        <div class="col-6">
-            <label for="formFile" class="form-label">Upload image</label>
-            <input class="form-control" type="file" id="formFile">
-        </div>
-        <div class="col-6">
-            <label for="formFile" class="form-label">Select country</label>
-            <select class="form-select" aria-label="Default select example">
-            <option selected>Country</option>
-            <option value="1">Kenya</option>
-            <option value="2">Uganda</option>
-            <option value="3">Burundi</option>
-            </select>
-        </div>
-    </div>
+    </form>
 </div>
