350f870857a7ace5042dc3e3cf18321af40c25aa
nardosteve <jackcale50@gmail.com>
2023-04-14 12:50:44 +0300

    TESTING:: Image upload

--- db_connection/insert_data.php
+++ db_connection/insert_data.php
@@ -5,13 +5,19 @@ include_once 'connection.php';
 if(isset($_POST['submit'])){
     $email = $_POST['email'];
     $names = $_POST['names'];
-    $uploadImage = $_POST['uploadImage'];
+
+    //Image handling
+    $uploadImage = $FILES['uploadImage']['name'];
+    $tempName = $FILES['uploadImage']['tmp_name'];
+    $folder = "../images/" . $uploadImage;
+    //Image handling
+
     $country = $_POST['country'];
 
     $sqlInsert = "INSERT INTO userform (emailAddress, fullNames, fullNames, country)
         VALUES('$email', '$names', '$uploadImage', '$country')";
 
-    if($conn->query($sqlInsert) === TRUE){
+    if($conn->query($sqlInsert) === TRUE && move_uploaded_file($tempName, $folder)){
         echo "User Added!";
     }else{
         echo " Error:" . $sqlInsert . "<br>" . $conn->error;
