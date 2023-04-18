200b589bb9a3fb46fa573cbf8ac0da361587d434
nardosteve <jackcale50@gmail.com>
2023-04-18 15:49:58 +0300

    Carousel

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
-    <?php include 'header.php' ?>
-    <div class="container">
-        <h1 class="h2 text-center mt-5">Form Upload</h1>
-        
+<?php include'header.php' ?>
+
+<div id="carouselExampleFade" class="carousel slide carousel-fade">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/1.jpg" class="d-block w-100" alt="...">
     </div>
-    <?php include 'footer.php' ?>
+    <div class="carousel-item">
+      <img src="images/2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<?php include 'footer.php' ?>
