e793ea296c0e4f3d2896ed21dc259bc7ec66d8f4
nardosteve <jackcale50@gmail.com>
2023-04-18 14:39:39 +0300

    Pulling data records from the DB

--- all-records.php
+++ all-records.php
@@ -1,32 +1,42 @@
 <?php include 'header.php' ?>
-<div class="container mt-5">
-    <table class="table table-striped">
-        <thead>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            </tr>
-            <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            </tr>
-            <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-            </tr>
+<?php include 'db_connection/connection.php' ?>
+
+<?php 
+    $selectAll = "SELECT * FROM userform;";
+    $results = $conn->query($selectAll);
+
+    echo '<div class="container mt-5">';
+    echo '<table class="table table-striped">';
+    echo '
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">File Upload</th>
+                <th scope="col">Country</th>
+                </tr>
+            </thead>
+        ';
+    echo '<tbody>';
+
+    if($results->num_rows > 0){
+        while($row = $results->fetch_assoc()){
+            // echo "id: ". $row["id"] . "<br>" . "Email Address: ". $row["emailAddress"];
+            echo '<tr>';
+            echo '<th scope="row">' . $row["id"] . '</th>';
+            echo '<td>' . $row["emailAddress"] . '</td>';
+            echo '<td>' . $row["fullNames"] . '</td>';
+            echo '<td>' . $row["uploadImage"] . '</td>';
+            echo '<td>' . $row["country"] . '</td>';
+            echo '</tr>';
+        }
+
+    }else{
+
+    }
+?>
+
         </tbody>
     </table>
 </div>
