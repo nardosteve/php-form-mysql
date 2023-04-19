4e171f3e4e5569432a9322e554d3bdf6a6dea212
nardosteve <jackcale50@gmail.com>
2023-04-19 14:55:13 +0300

    Sorting out PHP syntax double & single quotes to get data by ID

--- all-records.php
+++ all-records.php
@@ -11,8 +11,8 @@
             <thead>
                 <tr>
                 <th scope="col">ID</th>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
+                <th scope="col">Email Address</th>
+                <th scope="col">Full Names</th>
                 <th scope="col">File Upload</th>
                 <th scope="col">Country</th>
                 <th scope="col">Actions</th>
@@ -34,36 +34,77 @@
             echo '<td>' . $row["fullNames"] . '</td>';
             echo '<td>' . $row["uploadImage"] . '</td>';
             echo '<td>' . $row["country"] . '</td>';
-            echo '<div class="row">
-                    <td><a href="" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo"><img src="icons/eye.svg"></a></td>    
-                    <td><a href=""><img src="icons/pencil-square.svg"></a></td>
-                    <td><a href=""><img src="icons/trash.svg"></a></td>
-                </div<>';
+            echo '<div class="row">';
+            echo "<td><a href='#view". $row['id'] . "' data-bs-toggle='modal' data-bs-target='#view'><img src='icons/eye.svg'></a></td>";          
+            echo "<td><a href='#edit". $row['id'] . "' data-bs-toggle='modal' data-bs-target='#edit' data-bs-whatever='@mdo'><img src='icons/pencil-square.svg'></a></td>";        
+            echo '<td><a href=""><img src="icons/trash.svg"></a></td>';       
+            echo '</div<>';
             echo '</tr>';
 
+            //View Modal
             echo '
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal fade" id="view" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">User (#102480)</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <h1 class="h4">Email Address :</h1>
+                            <p>smungaimuroki@gmail.com</p>
+
+                            <h1 class="h4">Name :</h1>
+                            <p>Stephen M Muroki</p>
+
+                            <h1 class="h4">File Uploaded :</h1>
+                            <p>[.....]</p>
+
+                            <h1 class="h4">Country :</h1>
+                            <p>Kenya (KE)</p>
+                        </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            ';
+
+            //Edit Modal Form
+            echo '
+                <div class="modal fade" id="edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">User (#102480)</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                         <form>
                             <div class="mb-3">
-                            <label for="recipient-name" class="col-form-label">Recipient:</label>
-                            <input type="text" class="form-control" id="recipient-name">
+                                <label for="email" class="col-form-label">Email Address</label>
+                                <input type="text" class="form-control" id="email" name="email">
                             </div>
                             <div class="mb-3">
-                            <label for="message-text" class="col-form-label">Message:</label>
-                            <textarea class="form-control" id="message-text"></textarea>
+                                <label for="names" class="col-form-label">Full Names</label>
+                                <input type="text" class="form-control" id="names" name="names">
+                                
+                            </div>
+                            <div class="mb-3">
+                                <label for="uploadFile" class="col-form-label">Upload File</label>
+                                <input type="file" class="form-control" id="uploadFile" name="uploadFile">
+                            </div>
+                            <div class="mb-3">
+                                <label for="country" class="col-form-label">Country</label>
+                                <input type="text" class="form-control" id="country" name="country">
                             </div>
                         </form>
                         </div>
                         <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Send message</button>
+                        <button type="submit" class="btn btn-outline-primary">Update</button>
                         </div>
                     </div>
                     </div>
